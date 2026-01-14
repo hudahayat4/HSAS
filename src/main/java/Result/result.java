38aae1c21dfba1af463faf4707d7d922aebda3f3
2024963151-cpu <2024963151@student.uitm.edu.my>
2026-01-14 16:36:27 +0800

    enter appt class

---
+++ src/main/java/Result/result.java
@@ -0,0 +1,60 @@
+package Result;
+
+import java.sql.Date;
+import java.io.Serializable;
+
+public class result implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+
+	private int resultId;
+	private int appointmentId;
+	private Date resultDate;
+	private String resultComment;
+	
+	//default constructor 
+	public result() {}
+	
+	//parameterized constructor 
+	public result( int resultId, int appointmentId,
+	 Date resultDate, String resultComment) {
+		this.setResultId(resultId);
+		this.setAppointmentId(appointmentId);
+		this.setResultDate(resultDate);
+		this.setResultComment(resultComment);
+	}
+
+	public int getResultId() {
+		return resultId;
+	}
+
+	public void setResultId(int resultId) {
+		this.resultId = resultId;
+	}
+
+	public int getAppointmentId() {
+		return appointmentId;
+	}
+
+	public void setAppointmentId(int appointmentId) {
+		this.appointmentId = appointmentId;
+	}
+
+	public Date getResultDate() {
+		return resultDate;
+	}
+
+	public void setResultDate(Date resultDate) {
+		this.resultDate = resultDate;
+	}
+
+	public String getResultComment() {
+		return resultComment;
+	}
+
+	public void setResultComment(String resultComment) {
+		this.resultComment = resultComment;
+	}
+	
+}
